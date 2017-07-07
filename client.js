61168e99fd4f7293fc904453073c1adee17ec90c
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-06 20:29:05 -1000

    Almost done with server side.

--- client.js
+++ client.js
@@ -1,23 +1,23 @@
-const net = require('net');
-
-
-var client = new net.Socket();
-
-
-client.connect(8080, "0.0.0.0", () => {
-  console.log('Connected successfully to ' + client.address().address);
-});
-
-
-
-client.on('data', (data) => {
-  process.stdout.write(data);
-});
-
-
-process.stdin.on('readable', () => {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    client.write(chunk);
-  }
-});
+// const net = require('net');
+//
+//
+// var client = new net.Socket();
+//
+//
+// client.connect(8080, "0.0.0.0", () => {
+//   console.log('Connected successfully to ' + client.address().address);
+// });
+//
+//
+//
+// client.on('data', (data) => {
+//   process.stdout.write(data);
+// });
+//
+//
+// process.stdin.on('readable', () => {
+//   var chunk = process.stdin.read();
+//   if (chunk !== null) {
+//     client.write(chunk);
+//   }
+// });
--- server.js
+++ server.js
@@ -1,50 +1,50 @@
 const net = require('net');
-const fs= require('fs');
-const date= new Date()
+const fs = require('fs');
+const date = new Date()
 
 const error404 = fs.readFileSync('./public/404.html');
 const helium = fs.readFileSync('./public/helium.html');
 const hydrogen = fs.readFileSync('./public/hydrogen.html');
 const index = fs.readFileSync('./public/index.html');
-const css = fs.readFileSync('./public/styles.css');
-
-let server = net.createServer(function(socket){
-
-socket.on('data' , (data) => {
-  const myData = data.toString().split('\n');
-  const myArray =  myData[0].split(' ');
-  const path = myArray[1];
-console.log(path)
-  switch (path) {
-
-    case '/':
-     socket.write(insertData('200 OK', 'text/html', index));
-     socket.end();
-     break;
-
-    case '404.html':
-      socket.write(insertData('200 OK','text/html',error404));
-      console.log(path)
-      break;
-
-  }
 
 
+const server = net.createServer( (socket) => {
 
-});
+      socket.on('data', (data) => {
+          const myData = data.toString().split('\n');
+          const myArray = myData[0].split(' ');
+          const path = myArray[1];
+          console.log(myArray);
+          switch (path) {
 
+            case '/':
+              socket.write(insertData('200 OK', 'text/html', index));
+              socket.end();
 
+                break;
 
+            case '/helium.html':
+              socket.write(insertData('200 OK', 'text/html', helium));
+              socket.end();
 
+              break;
 
+            case '/hydrogen.html':
+              socket.write(insertData('200 OK', 'text/html', hydrogen));
+              socket.end();
 
+              break;
 
-socket.end();
+            default:
+              socket.write(insertData('404', 'text/html', error404));
+              socket.end();
 
+              }
 
+          });
 
-}).listen(8080)
+      }).listen(8080)
 
-function insertData(status,type,name){
-  return "HTTP/1.1 ${status} OK\nServer: nginx/1.4.6 (Ubuntu)\n" + date + "\nContent-Type: ${type}; charset=utf-8Content-Length: 10\nConnection: keep-alive\n\n${name}"
-}
+    function insertData(status, type, name) {
+      return `HTTP/1.1 ${status}\nServer: nginx/1.4.6 (Ubuntu)\n${date}\nContent-Type: ${type}; charset=utf-8Content-Length:${name.length}\nConnection: keep-alive\n\n${name}`
+    }
