8892015e514a1fd2cc296252043df4b2c72e5012
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-06 19:31:53 -1000

    Made HTML files trying to get files to go up.

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const net = require('net');
+const fs= require('fs');
+const date= new Date()
 
-let date= new Date()
-let code="200"
-
-let clients=[];
-let  
+const error404 = fs.readFileSync('./public/404.html');
+const helium = fs.readFileSync('./public/helium.html');
+const hydrogen = fs.readFileSync('./public/hydrogen.html');
+const index = fs.readFileSync('./public/index.html');
+const css = fs.readFileSync('./public/styles.css');
 
 let server = net.createServer(function(socket){
 
+socket.on('data' , (data) => {
+  const myData = data.toString().split('\n');
+  const myArray =  myData[0].split(' ');
+  const path = myArray[1];
+console.log(path)
+  switch (path) {
 
-console.log('Server')
+    case '/':
+     socket.write(insertData('200 OK', 'text/html', index));
+     socket.end();
+     break;
 
-socket.write("HTTP/1.1 200 OK\nServer: nginx/1.4.6 (Ubuntu)\n" + date + "\nContent-Type: text/html; charset=utf-8Content-Length: 10\nConnection: keep-alive\n\n<h1>hello</h1>")
+    case '404.html':
+      socket.write(insertData('200 OK','text/html',error404));
+      console.log(path)
+      break;
+
+  }
+
+
+
+});
+
+
+
+
+
+
 
 socket.end();
 
 
 
 }).listen(8080)
+
+function insertData(status,type,name){
+  return "HTTP/1.1 ${status} OK\nServer: nginx/1.4.6 (Ubuntu)\n" + date + "\nContent-Type: ${type}; charset=utf-8Content-Length: 10\nConnection: keep-alive\n\n${name}"
+}
